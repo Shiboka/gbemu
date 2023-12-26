7f1c5b6a641c0f89dd9ca5471dcc01f80acf781f
Shiboka <d.aubie@outlook.com>
2023-12-26 16:16:33 -0400

    added 0x20 and WIP Flags

--- src/main.nim
+++ src/main.nim
@@ -10,7 +10,7 @@ let
     rom_size = header[0x0148]
 
 var
-    A, B, C, D, E, H, L: uint8 = 0
+    A, B, C, D, E, H, L, F: uint8 = 0
     PC: uint16 = 0x0100
     memory: array[0xffff, uint8]
 
@@ -23,6 +23,7 @@ proc exec(): void =
             echo "jp ", toHex(PC)
         of 0xaf:
             A = A xor A
+            F = 0b1000_0000
             PC += 1
             echo "xor A"
         of 0x21:
@@ -40,14 +41,32 @@ proc exec(): void =
             HL -= 1;
             H = cast[uint8](HL shr 8)
             L = cast[uint8](HL)
+            PC += 1
             echo "ldd (hl), a: ", toHex(HL)
         of 0x06:
             B = memory[PC+1]
             PC += 2
             echo "ld b, ", toHex(B)
+        of 0x05:
+            B -= 1
+            if B == 0:
+                F = bitor(F, 0b1000_0000)
+            else:
+                F = bitand(F, 0b0111_0000)
+                F = bitor(F, 0b0100_0000)
+            # do something with H flag
+            PC += 1
+            echo "dec b: ", toHex(B)
+        of 0x20:
+            if bitand(F, 0b1000_0000) == 0b0000_0000:
+                PC += memory[PC+1]
+                echo "jr nz, ", toHex(memory[PC+1]), " TRUE"
+            else:
+                echo "jr nz, ", toHex(memory[PC+1]), " FALSE"
+                PC += 2
         else:
             echo toHex(memory[PC])
             PC += 1
 
-for i in 1..10:
+for i in 0..20:
     exec()
